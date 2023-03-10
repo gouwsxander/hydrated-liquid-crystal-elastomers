5c0ddddb453a694394247a51d304e0bd59c73e95
Xander <gouws.xander@gmail.com>
2023-03-10 15:36:26 -0400

    made everything arrays in plot_masic.py

--- 2023 Winter/Experimental data/plot_masic.py
+++ 2023 Winter/Experimental data/plot_masic.py
@@ -6,9 +6,9 @@ import pandas as pd
 df_r_strain = pd.read_csv('2023 Winter/Experimental data/masic_r_strain.csv')
 df_z_strain = pd.read_csv('2023 Winter/Experimental data/masic_z_strain.csv')
 
-relative_humidities = (df_r_strain["RH(%)"] + df_z_strain["RH(%)"])/2 / 100
-r_strains = df_r_strain["Strain(%)"] / 100
-z_strains = df_z_strain["Strain(%)"] / 100
+relative_humidities = np.array((df_r_strain["RH(%)"] + df_z_strain["RH(%)"])/2 / 100)
+r_strains = np.array(df_r_strain["Strain(%)"] / 100)
+z_strains = np.array(df_z_strain["Strain(%)"] / 100)
 
 r_deformations = 1 + r_strains
 z_deformations = 1 + z_strains
@@ -20,9 +20,16 @@ plt.ylabel("Deformation anisotropy $\lambda_r/\lambda_z$")
 
 plt.show()
 
-plt.scatter(r_deformations**2 * z_deformations, (r_deformations-1)/(z_deformations-1))
+# plt.scatter(r_deformations**2 * z_deformations, (r_deformations-1)/(z_deformations-1))
+
+# plt.xlabel("Swelling ratio $\lambda_r^2 \lambda_z$")
+# plt.ylabel("Strain anisotropy $\\varepsilon_r/\\varepsilon_z$")
+
+# plt.show()
+
+plt.scatter(r_deformations[:-1]**2 * z_deformations[:-1], (r_deformations[1:] - r_deformations[:-1])/(z_deformations[1:] - z_deformations[:-1]))
 
 plt.xlabel("Swelling ratio $\lambda_r^2 \lambda_z$")
-plt.ylabel("Strain anisotropy $\\varepsilon_r/\\varepsilon_z$")
+plt.ylabel("Deformation anisotropy $d\lambda_r/d\lambda_z$")
 
 plt.show()
