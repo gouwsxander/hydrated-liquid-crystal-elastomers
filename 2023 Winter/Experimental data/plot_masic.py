0e6611e46e43a9fc86330524e7ce73a2ddb42bb7
Xander <gouws.xander@gmail.com>
2023-03-21 13:27:52 -0300

    Added some extra plots for a meeting

--- 2023 Winter/Experimental data/plot_masic.py
+++ 2023 Winter/Experimental data/plot_masic.py
@@ -14,12 +14,33 @@ z_strains = np.array(df_z_strain["Strain(%)"] / 100)
 r_deformations = 1 + r_strains
 z_deformations = 1 + z_strains
 
+r_deformations_hydration = r_deformations / r_deformations[0]
+z_deformations_hydration = z_deformations / z_deformations[0]
+
 plt.scatter(r_deformations**2 * z_deformations, r_deformations/z_deformations)
 
-plt.xlabel("Swelling ratio $\lambda_r^2 \lambda_z$")
-plt.ylabel("Deformation anisotropy $\lambda_r/\lambda_z$")
+plt.xlabel("Dehydration swelling ratio $\lambda_r^2 \lambda_z$")
+plt.ylabel("Dehydration deformation anisotropy $\lambda_r/\lambda_z$")
 
 plt.show()
+
+plt.scatter(r_deformations_hydration**2 * z_deformations_hydration, r_deformations_hydration/z_deformations_hydration)
+
+plt.xlabel("Hydration swelling ratio $\lambda_r^2 \lambda_z$")
+plt.ylabel("Hydration deformation anisotropy $\lambda_r/\lambda_z$")
+
+plt.show()
+
+plt.scatter(r_deformations[:-1]**2 * z_deformations[:-1], r_strains[:-1] / z_strains[:-1])
+plt.xlabel("Dehydration swelling ratio $\lambda_r^2 \lambda_z$")
+plt.ylabel("Dehydration strain anisotropy $\\varepsilon_r/\\varepsilon_z$")
+plt.show()
+
+plt.scatter(r_deformations_hydration**2 * z_deformations_hydration, (r_deformations_hydration-1)/(z_deformations_hydration-1))
+plt.xlabel("Hydration swelling ratio $\lambda_r^2 \lambda_z$")
+plt.ylabel("Hydration strain anisotropy $\\varepsilon_r/\\varepsilon_z$")
+plt.show()
+
 
 # plt.scatter(r_deformations**2 * z_deformations, (r_deformations-1)/(z_deformations-1))
 
@@ -43,19 +64,37 @@ changes_in_mass = df["Total per dry"]
 
 saturations = changes_in_mass / 67 # chosen qualitatively
 
-sat_from_RH = lagrange(saturation_data_relative_humidities, saturations)
+sat_from_RH = lagrange(list(saturation_data_relative_humidities) + [0,1], list(saturations) + [0,1])
 
 plt.scatter(sat_from_RH(relative_humidities) * 100, r_deformations**2 * z_deformations)
 plt.xlabel("Saturation (%)")
-plt.ylabel("Swelling ratio $\\beta = \\lambda_r^2 \\lambda_z$")
+plt.ylabel("Dehydration swelling ratio $\\beta = \\lambda_r^2 \\lambda_z$")
+plt.show()
+
+plt.scatter(sat_from_RH(relative_humidities) * 100, r_deformations_hydration**2 * z_deformations_hydration)
+plt.xlabel("Saturation (%)")
+plt.ylabel("Hydration swelling ratio $\\beta = \\lambda_r^2 \\lambda_z$")
+plt.xlim(0,100)
 plt.show()
 
 plt.scatter(sat_from_RH(relative_humidities) * 100, r_deformations / z_deformations)
 plt.xlabel("Saturation (%)")
-plt.ylabel("Deformation anisotropy $\lambda_r/\lambda_z$")
+plt.ylabel("Dehydration deformation anisotropy $\lambda_r/\lambda_z$")
+plt.show()
+
+plt.scatter(sat_from_RH(relative_humidities) * 100, r_deformations_hydration/z_deformations_hydration)
+plt.xlabel("Saturation (%)")
+plt.ylabel("Hydration deformation anisotropy $\lambda_r/\lambda_z$")
+plt.xlim(0,100)
 plt.show()
 
 plt.scatter(sat_from_RH(relative_humidities[:-1]) * 100, r_strains[:-1] / z_strains[:-1])
 plt.xlabel("Saturation (%)")
-plt.ylabel("Strain anisotropy $\\varepsilon_r/\\varepsilon_z$")
+plt.ylabel("Dehydration strain anisotropy $\\varepsilon_r/\\varepsilon_z$")
+plt.show()
+
+plt.scatter(sat_from_RH(relative_humidities) * 100, (r_deformations_hydration-1)/(z_deformations_hydration-1))
+plt.xlabel("Saturation (%)")
+plt.ylabel("Hydration strain anisotropy $\\varepsilon_r/\\varepsilon_z$")
+plt.xlim(0,100)
 plt.show()
--- 2023 Winter/Experimental data/saturation_from_humidity.py
+++ 2023 Winter/Experimental data/saturation_from_humidity.py
@@ -10,9 +10,13 @@ changes_in_mass = df["Total per dry"]
 
 saturation = changes_in_mass / 67 # chosen qualitatively
 
-polynomial_fit = lagrange(relative_humidities, saturation)
+polynomial_fit = lagrange(list(relative_humidities) + [0,100], list(saturation) + [0,1])
 
-plt.scatter(relative_humidities, saturation)
-plt.plot(np.linspace(0,100), polynomial_fit(np.linspace(0,100)))
+plt.scatter(relative_humidities, 100*saturation)
+plt.scatter([0,100],[0,100])
+plt.plot(np.linspace(0, 100, 1000), 100 * polynomial_fit(np.linspace(0, 100, 1000)))
+
+plt.xlabel("Relative humidity (%)")
+plt.ylabel("Saturation (%)")
 
 plt.show()
